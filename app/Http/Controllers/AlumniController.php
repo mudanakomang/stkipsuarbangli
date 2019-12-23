75164ae433a95cc3587079bae51c0a2c7ab2e451
Mudana <mudanakomang@hotmail.com>
2019-12-24 01:32:41 +0800

    update + downgrade php

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -12,14 +12,21 @@ class AlumniController extends Controller
     }
     public function getalumni(Request $request){        
         $page =$request->page;
-        $limit=20;
-        $alumni=\App\Alumni::when($request->nama !='',function($q) use ($request){
-            return $q->where('nama','LIKE',"%$request->nama%")->orWhere('nim','LIKE',"%$request->nama%");
-        })->when($request->jurusan != '',function($q) use($request){
-            return $q->where('jurusan','=',$request->jurusan);
-        })->when($request->periode!='',function($q) use ($request){
-            return $q->where('periode','=',$request->periode);
+        $limit=20;          
+        $alumni=\App\Alumni::when($request->nama!=null  && $request->jurusan!=null  &&  $request->periode!=null,function($q) use ($request){
+            $q->where('nama','LIKE',"%$request->nama%")->where('jurusan','=',"$request->jurusan")->where('periode','=',"$request->periode");
+        })->when($request->nama!=null  && $request->jurusan!=null  &&  $request->periode==null,function($q) use($request){
+            $q->where('nama','LIKE',"%$request->nama%")->where('jurusan','=',"$request->jurusan");
+        })->when($request->nama!=null  && $request->jurusan==null  &&  $request->periode==null,function($q) use ($request){
+            $q->where('nama','LIKE',"%$request->nama%");
+        })->when($request->nama==null  && $request->jurusan==null  &&  $request->periode!=null,function($q) use ($request){
+            $q->where('periode','=',"$request->periode");
+        })->when($request->nama==null  && $request->jurusan!=null  &&  $request->periode!=null,function($q) use ($request){
+            $q->where('jurusan','=',"$request->jurusan")->where('periode','=',"$request->periode");
+        })->when($request->nama!=null  && $request->jurusan==null  &&  $request->periode!=null,function($q) use ($request){
+            $q->where('nama','LIKE',"%$request->nama%")->where('periode','=',"$request->periode");
         })->orderBy('nim','asc')->limit($limit)->offset(($page - 1) * $limit)->paginate($limit);
+       
         return response(['data'=>$alumni],200);
     }
 }
